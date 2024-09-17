75ad16d07128619e396679be9cc0fee2c9c2559b
julienRactM <julien.ract@sfr.fr>
2024-09-17 14:23:06 +0200

    [Update] Visualisation colorized

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # uneviedefourmi M1 Project
+
+
+## Suivi de projet
+
+-
+
+
+## To do list
+
+- Make sure SV is on the left side of plot and colorize Sv Sd
--- ants.py
+++ ants.py
@@ -14,9 +14,8 @@ class Anthill:
 
         # self.all_antshill = json.load("data/antshill.json")
         with open('data/antshill.json', 'r') as file:
-            # print(json_data.keys)
             self.json_data = json.load(file)[f'antshill_{file_number}']
-            print(self.json_data['Nodes'])
+            # print(self.json_data['Nodes'])
 
         self.nodes = self.json_data["Nodes"]
 
@@ -27,24 +26,45 @@ class Anthill:
         F = nx.Graph()
 
         # Adding Nodes first
+        color_map = []
         for node in self.nodes.keys():
-            # print(node)
+            print(node)
+
             F.add_node(node)
+
+            if node == "Sv":
+                color_map.append('#930ac9')
+            elif node == "Sd":
+                color_map.append('#7edb32')
+            else :
+                color_map.append('#1ea4d9')
 
         # Then adding edges
         for node in self.nodes.keys():
             for edge in self.nodes[node]:
                 F.add_edge(node, edge)
 
-        nx.draw(F, with_labels = True, node_size= 3000)
+
+    ### Fixed position makes questionable graphs, can be used for presentation thought.
+                # Generate positions using spring layout
+        # pos = nx.spring_layout(F)  # This generates default positions
+
+        # # Manually set "Sv" to the far left (e.g., x=-1.0)
+        # pos["Sv"] = [-1.0, 0.0]  # Move "Sv" to the left side
+
+        # # Optionally adjust the layout of other nodes if needed
+        # pos["Sd"] = [1.0, 0.0]  # You can adjust "Sd" or other nodes as well
 
 
-    def make_paths(self):
+        nx.draw(F, node_color=color_map, with_labels = True, node_size= 1200) # pos
+        plt.show()
+
+
+    def make_brute_path(self):
         count = 0
-        print(self.nodes["Sv"])
-        for possibility in self.nodes["Sv"]:
-            pass
 
+
+        # En gros
 
         # make the loop stop when reaching Sd
 
