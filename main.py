ec4cc257819285b665064cfed4694910eb8f793d
julienRactM <julien.ract@sfr.fr>
2024-09-23 10:50:25 +0200

    [Create] reqs.txt & main start file

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #pass
+from ants import Anthill
+
+if __name__ == "__main__":
+    anthill = Anthill(file_number=3)
+    anthill.init_movement()
+    anthill.simulate_ants_movement_with_multiple_paths("Sv", "Sd")
